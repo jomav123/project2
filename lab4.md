a4cbc7290ab78a9335121937b98bd900c65586ec
Jo Mav <djemsmavoungou@gmail.com>
2025-04-01 12:05:24 +0200

    two files updated

--- lab4.md
+++ lab4.md
@@ -1,1 +1,2 @@
-# project2
+print(f"let's create our workflow "
+      f"by defining a pipeline")
---
+++ lab4.py
@@ -0,0 +1,9 @@
+def multiplication():
+    num1 = int(input("enter the first number: "))
+    num2 = int(input("enter the second number: "))
+    result = num1 * num2
+    print(f"{num1}x{num2}= {result}")
+    return result
+
+
+multiplication()
